1d98fe813562cfbf38a738c13174318f62bfc971
Pawel Mac <Pawel.Mac@gmail.com>
2017-12-15 19:36:15 +0100

    Create class ViewportModel

---
+++ src/main/java/com/mycompany/virtual_camera/model/ViewportModel.java
@@ -0,0 +1,59 @@
+package com.mycompany.virtual_camera.model;
+
+import java.awt.geom.Line2D;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import org.apache.commons.math3.linear.RealMatrix;
+
+/**
+ *
+ * @author Paweł Mac
+ */
+public class ViewportModel {
+    
+    private Set<Point3D> point3DsSet;
+    private Set<Edge3D> edge3DsSet;
+    private double distanceBetweenObserverAndViewport;
+    private final int viewportWidth;
+    private final int viewportHeight;
+    
+    private Map<Edge3D, Line2DHolder> edge3DToLine2DHolderMap;
+    
+    private RealMatrix perspectiveTransformationMatrix;
+    
+    public ViewportModel(Set<Point3D> point3DsSet, Set<Edge3D> edge3DsSet, double distanceBetweenObserverAndViewport, int viewportWidth, int viewportHeight) {
+        this.point3DsSet = point3DsSet;
+        this.edge3DsSet = edge3DsSet;
+        this.distanceBetweenObserverAndViewport = distanceBetweenObserverAndViewport;
+        this.viewportWidth = viewportWidth;
+        this.viewportHeight = viewportHeight;
+        
+        this.edge3DToLine2DHolderMap = new HashMap<>();
+    }
+    
+    // Getters and Setters
+    
+    // Methods
+    
+    private void initEdge3DToLine2DHolderMap() {
+        for (Edge3D edge3D : edge3DsSet) {
+            Line2D line2D = new Line2D.Double(0.0d, 0.0d, 0.0d, 0.0d);
+            Line2DHolder line2DHolder = new Line2DHolder(line2D);
+            this.calculateLine2DOnViewport(edge3D, line2DHolder);
+            this.edge3DToLine2DHolderMap.put(edge3D, line2DHolder);
+        }
+    }
+    
+    private void calculateLine2DOnViewport(Edge3D edge3D, Line2DHolder line2DHolder) {
+        double x1 = (edge3D.getFirst() .getX() * distanceBetweenObserverAndViewport) / edge3D.getFirst() .getZ();
+        double y1 = (edge3D.getFirst() .getY() * distanceBetweenObserverAndViewport) / edge3D.getFirst() .getZ();
+        double x2 = (edge3D.getSecond().getX() * distanceBetweenObserverAndViewport) / edge3D.getSecond().getZ();
+        double y2 = (edge3D.getSecond().getY() * distanceBetweenObserverAndViewport) / edge3D.getSecond().getZ();
+        x1 =  x1 + (viewportWidth  / 2.0d);
+        y1 = -y1 + (viewportHeight / 2.0d);
+        x2 =  x2 + (viewportWidth  / 2.0d);
+        y2 = -y2 + (viewportHeight / 2.0d);
+        line2DHolder.getLine2D().setLine(x1, y1, x2, y2);
+    }
+}
