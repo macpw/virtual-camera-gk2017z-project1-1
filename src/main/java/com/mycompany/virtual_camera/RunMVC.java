04f5944a1492b402b1608c1fe8f1c453aaf16fc5
Pawel Mac <Pawel.Mac@gmail.com>
2017-12-15 19:58:07 +0100

    Create class RunMVC

---
+++ src/main/java/com/mycompany/virtual_camera/RunMVC.java
@@ -0,0 +1,16 @@
+package com.mycompany.virtual_camera;
+
+/**
+ *
+ * @author Paweł Mac
+ */
+public class RunMVC implements Runnable {
+    
+    public RunMVC() {
+    }
+    
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
