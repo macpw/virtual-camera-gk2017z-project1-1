0cc37a07ac501fe3b7c413986ecd8807f44d1303
Pawel Mac <Pawel.Mac@gmail.com>
2017-12-16 23:50:27 +0100

    Create class MoveForwardAction

---
+++ src/main/java/com/mycompany/virtual_camera/controller/motion/MoveForwardAction.java
@@ -0,0 +1,24 @@
+package com.mycompany.virtual_camera.controller.motion;
+
+import com.mycompany.virtual_camera.model.ViewportModel;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+
+/**
+ *
+ * @author Paweł Mac
+ */
+public class MoveForwardAction extends AbstractAction {
+    
+    ViewportModel viewportModel;
+    
+    public MoveForwardAction(ViewportModel viewportModel) {
+        this.viewportModel = viewportModel;
+        this.putValue(NAME, NAME);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        this.viewportModel.moveForward();
+    }
+}
