4f59a6b52d6b11ef9cc73a793870cb43f08000fb
Pawel Mac <Pawel.Mac@gmail.com>
2017-12-17 00:17:17 +0100

    Update Controller

--- src/main/java/com/mycompany/virtual_camera/controller/Controller.java
+++ src/main/java/com/mycompany/virtual_camera/controller/Controller.java
@@ -1,7 +1,9 @@
 package com.mycompany.virtual_camera.controller;
 
+import com.mycompany.virtual_camera.controller.motion.MoveForwardAction;
 import com.mycompany.virtual_camera.model.ViewportModel;
 import com.mycompany.virtual_camera.view.View;
+import javax.swing.JButton;
 
 /**
  *
@@ -15,5 +17,20 @@ public class Controller {
     public Controller(ViewportModel viewportModel, View view) {
         this.viewportModel = viewportModel;
         this.view = view;
+        this.addListenersToMotionButtons();
+    }
+    
+    private void addListenersToMotionButtons() {
+        JButton moveForwardJButton  = view.getMotionControlJPanel().getMoveForwardJButton();
+        JButton moveBackwardJButton = view.getMotionControlJPanel().getMoveBackwardJButton();
+        JButton moveLeftJButton     = view.getMotionControlJPanel().getMoveLeftJButton();
+        JButton moveRightJButton    = view.getMotionControlJPanel().getMoveRightJButton();
+        JButton moveUpwardJButton   = view.getMotionControlJPanel().getMoveUpwardJButton();
+        JButton moveDownwardJButton = view.getMotionControlJPanel().getMoveDownwardJButton();
+        
+        MoveForwardAction moveForwardAction = new MoveForwardAction(viewportModel);
+        
+        moveForwardJButton.setAction(moveForwardAction);
+        //moveForwardJButton.addActionListener(moveForwardAction);
     }
 }
